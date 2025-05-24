2a9a732be0b192e4c5fd933f7711043104a8e69f
Prejan <neupaneprejan2018@gmail.com>
2025-05-24 16:24:14 +0545

    add requirements

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,8 +2,6 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-
-
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
