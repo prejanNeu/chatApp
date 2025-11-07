885975744715d45b0884fc563477c394527906af
Prabesh Subedi <prabesh.sb@gmail.com>
2025-11-07 17:45:26 +0545

    make migrations

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, BaseUserManager, AbstractBaseUser, PermissionsMixin
-import uuid 
+import uuid
+
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, full_name, password=None):
@@ -25,57 +26,59 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, default=None)
     full_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    is_delete = models.BooleanField(default=False) # flag for soft delete the customuser 
+    # flag for soft delete the customuser
+    is_delete = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = [ 'full_name']
+    REQUIRED_FIELDS = ['full_name']
 
     objects = CustomUserManager()
 
-    class Meta :
-        ordering = ['timestamp']
+    # class Meta :
+    #     ordering = ['timestamp']
 
     def __str__(self):
         return self.username
 
+
 class ChatRoom(models.Model):
-    room_id = models.CharField(max_length=255, unique=True, editable=False)
+    room_id = models.CharField(
+        max_length=255, unique=True, editable=False, default=1)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    users = models.ManyToManyField(CustomUser, related_name="chat_rooms")\
-        
+    users = models.ManyToManyField(CustomUser, related_name="chat_rooms")
+
     is_delete = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return self.name
-    
-    
-    def save(self, *args, **kwargs): #generate the unique room_id while creating the room 
+
+    def save(self, *args, **kwargs):  # generate the unique room_id while creating the room
         if not self.room_id:
             self.room_id = str(uuid.uuid4())
         super().save(*args, **kwargs)
-        
-    
+
 
 class Message(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
-    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    room = models.ForeignKey(
+        ChatRoom, on_delete=models.CASCADE, related_name='messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    
+
     is_file = models.BooleanField(default=False)
     is_delete = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    
-    class Meta :
+
+    class Meta:
         ordering = ['timestamp']
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}"
-
