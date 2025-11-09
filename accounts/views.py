5b43910d9bf6d686a5855d429ec844e0a984886a
Prabesh Subedi <prabesh.sb@gmail.com>
2025-11-09 16:23:35 +0545

    fix merge conflict

--- accounts/views.py
+++ accounts/views.py
@@ -37,7 +37,7 @@ def register(request):
                 request, "Registration successful! Please log in.")
             return redirect("login")
 
-        except IntegrityError as e :
+        except IntegrityError as e:
             messages.error(request, "Username or email already exists.")
             messages.error(request, f"{email} already exits?")
             print(e)
