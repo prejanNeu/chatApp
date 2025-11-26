bfab2af3d2738d2d369b8f2b28be141ca2220c0e
Prabesh Subedi <prabesh.sb@gmail.com>
2025-11-26 10:35:47 +0545

    serve media files

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve as static_serve
 
 from chat import views as chat_views
 
@@ -19,5 +20,9 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+
 # TODO: Need a dedicated static file server
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path("media/<path:path>", static_serve,
+         {"document_root": settings.MEDIA_ROOT}),
+]
