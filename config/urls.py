c958e312fab1ac76e3671eecc6e88ccfb2949200
Prabesh Subedi <prabesh.sb@gmail.com>
2025-11-26 10:07:20 +0545

    force django to server media files

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,14 +10,14 @@ urlpatterns = [
     path("chat/", include("chat.urls")),
     path("accounts/", include("accounts.urls")),
     path("friends/", include("friends.urls")),
-    
+
     path("admin/", admin.site.urls),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 # Serve static and media files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+# TODO: Need a dedicated static file server
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
